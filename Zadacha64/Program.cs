adbcb3a6fc60753a64f8daa733c50499d3afc360
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-26 15:26:16 +0300

    add Zadacha 64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. */
+
+void PlusFunt(int m, int n, int sum = 0)
+{
+    if (m == n)
+    {
+        Console.Write(sum + n);
+        return;
+    }
+    PlusFunt(m + 1, n, sum + m);
+}
+PlusFunt(4, 8);
